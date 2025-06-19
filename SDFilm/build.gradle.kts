b0b7ddcdae03730ca587b459ad9ed58efef7c020
dah215 <duongah514hp@gmail.com>
2025-06-19 10:41:09 +0700

    Add files via upload

---
+++ SDFilm/build.gradle.kts
@@ -0,0 +1,22 @@
+version = 10
+
+cloudstream {
+
+    description = "Aho (Lang) SDFilm"
+    language    = "vi"
+    authors = listOf("Aho")
+
+    /**
+    * Status int as the following:
+    * 0: Down
+    * 1: Ok
+    * 2: Slow
+    * 3: Beta only
+    * */
+    status = 1 
+    
+    tvTypes = listOf("SDFilm")
+    iconUrl = "icon url"
+
+    isCrossPlatform = true
+}
